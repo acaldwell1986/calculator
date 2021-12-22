7bc4839f7f3c32833564104bb9d564015229991a
acaldwell1986 <acaldwell1986@gmail.com>
2021-12-22 17:40:22 -0500

    Added operate function

--- calculator.js
+++ calculator.js
@@ -12,7 +12,6 @@ function multiply(a,b) {
 
 function divide(a,b) {
     let quotient;
-    console.log(b)
     if (b === 0) {
         return "Cannot divide by zero!"
     }
@@ -22,9 +21,18 @@ function divide(a,b) {
 }
 
 function operate(operator, num1, num2) {
-    if (operator === `+`) {
-        return add(num1,num2)
+    switch (operator) {
+        case `+`:
+            return add(num1,num2)
+        case `-`:
+            return subtract(num1,num2)
+        case `*`:
+            return multiply(num1,num2)
+        case `/`:
+            return divide(num1,num2)
+        default:
+            return `Something went wrong.`
     }
 }
 
-//console.log(operate(`+`, 1, 2));
+console.log(operate(`/`, 1, 2));
