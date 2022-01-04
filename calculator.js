92d1ce7a4667d8d4d44fbd349fa90cb74384d29f
acaldwell1986 <acaldwell1986@gmail.com>
2022-01-04 17:13:13 -0500

    Display updates

--- calculator.js
+++ calculator.js
@@ -1,8 +1,5 @@
 const buttons = document.querySelectorAll('button');
-const calcDisplay = document.querySelector('.calc-display');
-
-
-
+const calcDisplay = document.querySelector('div.calc-display');
 
 buttons.forEach(button => { button.addEventListener('click', operatorChoice) });
 
@@ -17,37 +14,65 @@ let numString2 = '';
 
 function operatorChoice(e) {
     const opReturn = e.target.innerHTML;
-
-    if (isNaN(opReturn)) {
+    
+    if (opReturn === '=' && operatorChosen === true) {
+        console.log(operate(operator, +numString1, +numString2))
+        num3 = operate(operator, +numString1, +numString2);
+        calcDisplay.innerHTML = num3;
+    }
+    else if (isNaN(opReturn) && opReturn !== '=') {
         operator = e.target.innerHTML;
         operatorChosen = true;
-
-
-        //addswitch for special operations
-        if (e.target.innerHTML = 'C') {
+        
+        //add switch for special operations?
+        if (e.target.innerHTML === 'C') {
             reset();
         }
     }
-    else {
+
+    else if (!isNaN(opReturn)){
+        //first pass
         if ((typeof num1 === 'undefined') && (typeof num2 === 'undefined') && (operatorChosen === false)) {
             num1 = +e.target.innerHTML;
             console.log(num1);
             calcDisplay.innerHTML = num1;
             numString1 = num1.toString();
         } 
-        else if (typeof num2 === 'undefined' && operatorChosen === false) {
-            console.log(num1)
-            console.log(numString1)
+
+        //second pass once more than 1 digit is added
+        else if (typeof num2 === 'undefined' && operatorChosen === false && numString1.length < 10) {
             num1 = +e.target.innerHTML;
             let temp = num1.toString();
             numString1 = temp.concat('', numString1);
             calcDisplay.innerHTML = numString1;
         }
+
+        //nothing happens if more than 9 digits are added to first value
+        else if (numString1.length >= 10 && operatorChosen === false) {
+            //nothing happens
+        }
+        //first digit added to second value
         else if ((typeof num1 !== 'undefined') && (typeof num2 === 'undefined')) {
             num2 = +e.target.innerHTML;
             console.log(num2);
             calcDisplay.innerHTML = num2;
-        } else {
+            numString2 = num2.toString();
+        } 
+        //additional passes until length reach 10
+        else if (numString2.length < 10) {
+            num2 = +e.target.innerHTML;
+            let temp = num2.toString();
+            numString2 = temp.concat('', numString2);
+            calcDisplay.innerHTML = numString2;
+        }
+        
+
+        else if (numString2 >= 10) {
+            //nothing happens
+        }
+
+        //handles all other cases
+        else {
             console.log('what')
         }
     }
@@ -56,13 +81,12 @@ function operatorChoice(e) {
 function reset() {
     operator = '';
     operatorChosen = false;
-    num1 = undefined
-    num2 = undefined
-    num3 = undefined
-    calcDisplay.innerHTML = '|'
+    num1 = undefined;
+    num2 = undefined;
+    num3 = undefined;
+    calcDisplay.innerHTML = '|';
     numString1 = '';
     numString2 = '';
-
 }
 
 function add(a,b) {
@@ -88,6 +112,7 @@ function divide(a,b) {
 }
 
 function operate(operator, num1, num2) {
+    console.log(operator, num1, num2)
     switch (operator) {
         case `+`:
             return add(num1,num2)
@@ -95,15 +120,14 @@ function operate(operator, num1, num2) {
             return subtract(num1,num2)
         case `*`:
             return multiply(num1,num2)
+        case `x`:
+            return multiply(num1,num2)
         case `/`:
             return divide(num1,num2)
         default:
             return `Something went wrong.`
     }
 }
-
-
-
 
 
 //console.log(operate(`-`, 8, 3));
