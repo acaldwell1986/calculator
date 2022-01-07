1171650dd4715079e3dfb08814734536bfdf3f41
acaldwell1986 <acaldwell1986@gmail.com>
2022-01-07 11:49:41 -0500

    test

--- calculator.js
+++ calculator.js
@@ -9,13 +9,49 @@ buttons.forEach(button => { button.addEventListener('click', operatorChoice) });
 // -rewrite this mofo
 
 
+// if (operatorChosen !== true) {
+//     operator = e.target.innerHTML;
+//     operatorChosen = true;
+//    }
+    
+//     //add switch for special operations?
+    
+
+// }
+// //handles logic for populating the display
+// else if (!isNaN(opReturn)){
+//     //first pass
+//     if ((typeof num1 === 'undefined') && (typeof num2 === 'undefined') && (operatorChosen === false)) {
+//         num1 = +e.target.innerHTML;
+//         console.log(num1);
+//         calcDisplay.innerHTML = num1;
+//         numString1 = num1.toString();
+//     } 
+
+const numButtons = document.querySelectorAll('button.number-button');
+const operatorButtons = document.querySelectorAll('button.operator-button');
+const specialButtons = document.querySelectorAll('button.special-button');
+const clearButton = document.querySelectorAll('button.clear-button')
+const equalButton = document.querySelectorAll('button.equal-button')
+const decimalButton = document.querySelectorAll('button.decimal-button')
+
+const calcDisplay = document.querySelector('div.calc-display');
+
+numButtons.forEach(button => { button.addEventListener('click', main) });
+
+operatorButtons.forEach(button => { button.addEventListener('click', onClicker) });
+specialButtons.forEach(button => { button.addEventListener('click', onClicker) });
+clearButton.forEach(button => { button.addEventListener('click', onClicker) });
+equalButton.forEach(button => { button.addEventListener('click', onClicker) });
+decimalButton.forEach(button => { button.addEventListener('click', onClicker) });
 
 let operator = '';
 let operatorChosen = false;
-let num1;
+let num1 = 0;
 let num2;
 let result;
 let nextResult; //not affected by reset
+let nextResult; 
 let numString1 = '';
 let numString2 = '';
 
@@ -153,6 +189,24 @@ function operatorChoice(e) {
         }
     }
 }
+
+
+
+function main(e) {
+    if (num1.toString.length < 10) {
+        num1 = +e.target.innerHTML;
+        console.log(num1);
+        calcDisplay.innerHTML = num1;
+        numString1 = num1.toString();
+    }
+}
+
+
+function onClicker(e) {
+    console.log(e.target.innerHTML);
+}
+
+
 
 
 
